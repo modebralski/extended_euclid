dcbea08de7d04292fdb8a6784c4576143972ed64
modebralski <m.odebralski@gmail.com>
2023-10-26 00:38:42 +0200

    added extended_euclid_step() function

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 def main():
-    print(f"Hi,")
+    a_value = 75
+    b_value = 28
+    extended_euclid_step(a_value, b_value)
+
+
+def extended_euclid_step(a_val: int, b_val: int, t0_val: int = 0, t_val: int = 1, first_step: bool = True):
+    q_val = int(a_val / b_val)
+    r_val = a_val % b_val
+    if first_step == False:
+        t_val = t0_val - q_val * t_val
+    print(f"a: {a_val} b: {b_val} r: {r_val} t0: {t0_val} t: {t_val} q: {q_val}")
+    return [a_val, b_val, r_val, t0_val, t_val, q_val]
 
 
 if __name__ == "__main__":
