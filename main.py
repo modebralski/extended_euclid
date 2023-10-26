6db7c19a25ce14052b61eafacaa686669346c1dd
modebralski <m.odebralski@gmail.com>
2023-10-26 02:04:40 +0200

    modified extended_euclid_step() function

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 def main():
-    a_value = 75
-    b_value = 28
-    extended_euclid_step(a_value, b_value)
+    a_value, mod_value = 61, 61
+    b_value = 17
+    while True:
+        a_value, b_value, t0_value, t_value, q_value, r_value = extended_euclid_step(mod_value, a_value, b_value)
+        if r_value != 0:
+            a_value, b_value, t0_value, t_value, q_value, r_value = extended_euclid_step(mod_val=mod_value, a_val=a_value,
+                                                                                         b_val=b_value, q_val=q_value,
+                                                                                         t0_val=t0_value, t_val=t_value,
+                                                                                         first_step=False)
+        else:
+            return False
 
 
-def extended_euclid_step(a_val: int, b_val: int, t0_val: int = 0, t_val: int = 1, first_step: bool = True):
-    q_val = int(a_val / b_val)
-    r_val = a_val % b_val
-    if first_step == False:
-        t_val = t0_val - q_val * t_val
-    print(f"a: {a_val} b: {b_val} r: {r_val} t0: {t0_val} t: {t_val} q: {q_val}")
-    return [a_val, b_val, r_val, t0_val, t_val, q_val]
+def extended_euclid_step(mod_val, a_val, b_val, q_val=0, t0_val=0, t_val=1, first_step=True):
+    r_value = a_val % b_val
+    q_value = int(a_val / b_val)
+    if not first_step:
+        t0_value = t_val
+        t_value = (t0_val - q_val * t_val) % mod_val
+    else:
+        t_value = t_val
+        t0_value = t0_val
+    print(f"a: {a_val} b: {b_val}, r: {r_value} t0: {t0_value} t: {t_value} q: {q_value}")
+    return [b_val, r_value, t0_value, t_value, q_value, r_value]
 
 
 if __name__ == "__main__":
