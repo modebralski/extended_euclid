1ca7f050e48e19b636bd67d937106e97070b77d6
modebralski <m.odebralski@gmail.com>
2023-10-26 02:40:12 +0200

    added extended_euclid() function added calculate_x() function

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-def main():
-    a_value = mod_value = 93
-    b_value = 41
+def extended_euclid(a_val, b_val):
+    a_value = mod_value = a_val
+    b_value = b_val
     a_value, b_value, t0_value, t_value, q_value = extended_euclid_step(mod_value, a_value, b_value)
     r_value = b_value
     while r_value != 0:
@@ -9,6 +9,7 @@ def main():
                                                                             t0_val=t0_value, t_val=t_value,
                                                                             first_step=False)
         r_value = b_value
+    return [a_val, b_val, t_value]
 
 
 def extended_euclid_step(mod_val, a_val, b_val, q_val=0, t0_val=0, t_val=1, first_step=True):
@@ -22,6 +23,16 @@ def extended_euclid_step(mod_val, a_val, b_val, q_val=0, t0_val=0, t_val=1, firs
         t_value = t_val
     print(f"a: {a_val} b: {b_val}, r: {r_value} t0: {t0_value} t: {t_value} q: {q_value}")
     return [b_val, r_value, t0_value, t_value, q_value]
+
+
+def calculate_x(a_val, b_val, t_val):
+    return (t_val * b_val - 1) / a_val
+
+
+def main():
+    a_value = 93
+    b_value = 41
+    print(calculate_x(*extended_euclid(a_value, b_value)))
 
 
 if __name__ == "__main__":
